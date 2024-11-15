c8edcc9460c89e123a0c934959a18ad3c5aeffdc
apple <105837597+380583067@users.noreply.github.com>
2024-11-14 22:52:37 -0800

    miui

---
+++ mocha/firmware.mk
@@ -0,0 +1,5 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/mocha/blob:install/firmware-update/blob \
+    vendor/xiaomi-firmware//mocha/misc.img:install/firmware-update/misc.img \
+    vendor/xiaomi-firmware/mocha/tos.img:install/firmware-update/tos.img
